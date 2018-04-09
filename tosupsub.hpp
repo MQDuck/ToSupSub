7a9a441d82b509688e615d2554104df2a0c6567f
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2018-04-08 17:07:12 -0700

    Combine to a single header file. Hide most function templates inside namespace tosupsub.

--- tosupsub.hpp
+++ tosupsub.hpp
@@ -11,17 +11,109 @@
 
 #include <string>
 
-namespace
+namespace tosupsub
 {
-#include "tosupsubscript_templates.hpp"
-} // anonymous namespace
+
+static const std::string digitToSuperscript[] {
+        "\u2070", // [0]  = 0
+        "\u00B9", // [1]  = 1
+        "\u00B2", // [2]  = 2
+        "\u00B3", // [3]  = 3
+        "\u2074", // [4]  = 4
+        "\u2075", // [5]  = 5
+        "\u2076", // [6]  = 6
+        "\u2077", // [7]  = 7
+        "\u2078", // [8]  = 8
+        "\u2079", // [9]  = 9
+        "\u207A", // [10] = +
+        "\u207B", // [11] = -
+        "\u22C5"  // [12] = . (sort of)
+};
+
+static const std::string digitToSubscript[] {
+        "\u2080", // [0]  = 0
+        "\u2081", // [1]  = 1
+        "\u2082", // [2]  = 2
+        "\u2083", // [3]  = 3
+        "\u2084", // [4]  = 4
+        "\u2085", // [5]  = 5
+        "\u2086", // [6]  = 6
+        "\u2087", // [7]  = 7
+        "\u2088", // [8]  = 8
+        "\u2089", // [9]  = 9
+        "\u208A", // [10] = +
+        "\u208B", // [11] = -
+        "."       // [12] = . (literally)
+};
+
+template<typename I, const std::string* const digitMap>
+std::string i_tosupsubscript(I number, std::string& supsubStr)
+{
+    I digitSize;
+    for(digitSize = 10; number / digitSize; digitSize *= 10);
+    for(digitSize /= 10; digitSize > 1; digitSize /= 10)
+    {
+        supsubStr += digitMap[number / digitSize];
+        number %= digitSize;
+    }
+    supsubStr += digitMap[number];
+
+    return supsubStr;
+};
+
+template<typename SI, const std::string* const digitMap>
+std::string si_tosupsubcript(SI number)
+{
+    std::string supsubStr;
+    if(number < 0)
+    {
+        number = -number;
+        supsubStr = digitMap[11];
+    }
+    return i_tosupsubscript<SI, digitMap>(number, supsubStr);
+};
+
+template<typename UI, const std::string* const digitMap>
+std::string ui_tosupsubcript(UI number)
+{
+    std::string supsubStr;
+    return i_tosupsubscript<UI, digitMap>(number, supsubStr);
+};
+
+template<typename F, const std::string* const digitMap>
+std::string f_tosupsubscript(F number)
+{
+    std::string numStr = std::to_string(number);
+    std::string supsubStr;
+    for(char& d : numStr)
+    {
+        switch(d)
+        {
+            case '+':
+                supsubStr += digitMap[10];
+                break;
+            case '-':
+                supsubStr += digitMap[11];
+                break;
+            case '.':
+                supsubStr += digitMap[12];
+                break;
+            default:
+                supsubStr += digitMap[d - '0'];
+        }
+    }
+
+    return supsubStr;
+};
+
+} // namespace tosupsub
 
 template<class T>
 std::string tosuperscript(const T number,
                           typename std::enable_if<std::is_integral<T>::value>::type* = 0,
                           typename std::enable_if<std::is_signed<T>::value>::type* = 0)
 {
-    return si_tosupsubcript<T, digitToSuperscript>(number);
+    return tosupsub::si_tosupsubcript<T, tosupsub::digitToSuperscript>(number);
 }
 
 template<class T>
@@ -29,13 +121,14 @@ std::string tosuperscript(const T number,
                           typename std::enable_if<std::is_integral<T>::value>::type* = 0,
                           typename std::enable_if<std::is_unsigned<T>::value>::type* = 0)
 {
-    return ui_tosupsubcript<T, digitToSuperscript>(number);
+    return tosupsub::ui_tosupsubcript<T, tosupsub::digitToSuperscript>(number);
 }
 
 template<class T>
-std::string tosuperscript(const T number, typename std::enable_if<std::is_floating_point<T>::value >::type* = 0)
+std::string tosuperscript(const T number,
+                          typename std::enable_if<std::is_floating_point<T>::value>::type* = 0)
 {
-    return f_tosupsubscript<T, digitToSuperscript>(number);
+    return tosupsub::f_tosupsubscript<T, tosupsub::digitToSuperscript>(number);
 }
 
 template<class T>
@@ -43,7 +136,7 @@ std::string tosubscript(const T number,
                           typename std::enable_if<std::is_integral<T>::value>::type* = 0,
                           typename std::enable_if<std::is_signed<T>::value>::type* = 0)
 {
-    return si_tosupsubcript<T, digitToSubscript>(number);
+    return tosupsub::si_tosupsubcript<T, tosupsub::digitToSubscript>(number);
 }
 
 template<class T>
@@ -51,13 +144,14 @@ std::string tosubscript(const T number,
                           typename std::enable_if<std::is_integral<T>::value>::type* = 0,
                           typename std::enable_if<std::is_unsigned<T>::value>::type* = 0)
 {
-    return ui_tosupsubcript<T, digitToSubscript>(number);
+    return tosupsub::ui_tosupsubcript<T, tosupsub::digitToSubscript>(number);
 }
 
 template<class T>
-std::string tosubscript(const T number, typename std::enable_if<std::is_floating_point<T>::value >::type* = 0)
+std::string tosubscript(const T number,
+                        typename std::enable_if<std::is_floating_point<T>::value >::type* = 0)
 {
-    return f_tosupsubscript<T, digitToSubscript>(number);
+    return tosupsub::f_tosupsubscript<T, tosupsub::digitToSubscript>(number);
 }
 
 #endif //TOSUPSUB_TOSUPSUB_HPP
