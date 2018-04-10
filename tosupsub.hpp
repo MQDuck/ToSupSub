e922e65cfbcacb7309a7efb4b17b72be73ed0105
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2018-04-10 13:46:52 -0700

    Modify string versions to accept all alphanumeric characters, using map<char, string>.

--- tosupsub.hpp
+++ tosupsub.hpp
@@ -11,6 +11,7 @@
 
 #include <string>
 #include <cstring>
+#include <map>
 
 namespace tosupsub
 {
@@ -44,6 +45,148 @@ namespace tosupsub
             "\u208A", // [10] = +
             "\u208B", // [11] = -
             "."       // [12] = . (literally)
+    };
+
+    static const std::map<char, std::string> charToSuperscript {
+            {'0', "⁰"},
+            {'1', "¹"},
+            {'2', "²"},
+            {'3', "³"},
+            {'4', "⁴"},
+            {'5', "⁵"},
+            {'6', "⁶"},
+            {'7', "⁷"},
+            {'8', "⁸"},
+            {'9', "⁹"},
+            {'a', "ᵃ"},
+            {'b', "ᵇ"},
+            {'c', "ᶜ"},
+            {'d', "ᵈ"},
+            {'e', "ᵉ"},
+            {'f', "ᶠ"},
+            {'g', "ᵍ"},
+            {'h', "ʰ"},
+            {'i', "ᶦ"},
+            {'j', "ʲ"},
+            {'k', "ᵏ"},
+            {'l', "ˡ"},
+            {'m', "ᵐ"},
+            {'n', "ⁿ"},
+            {'o', "ᵒ"},
+            {'p', "ᵖ"},
+            {'q', "ᑫ"},
+            {'r', "ʳ"},
+            {'s', "ˢ"},
+            {'t', "ᵗ"},
+            {'u', "ᵘ"},
+            {'v', "ᵛ"},
+            {'w', "ʷ"},
+            {'x', "ˣ"},
+            {'y', "ʸ"},
+            {'z', "ᶻ"},
+            {'A', "ᴬ"},
+            {'B', "ᴮ"},
+            {'C', "ᶜ"},
+            {'D', "ᴰ"},
+            {'E', "ᴱ"},
+            {'F', "ᶠ"},
+            {'G', "ᴳ"},
+            {'H', "ᴴ"},
+            {'I', "ᴵ"},
+            {'J', "ᴶ"},
+            {'K', "ᴷ"},
+            {'L', "ᴸ"},
+            {'M', "ᴹ"},
+            {'N', "ᴺ"},
+            {'O', "ᴼ"},
+            {'P', "ᴾ"},
+            {'Q', "Q"},
+            {'R', "ᴿ"},
+            {'S', "ˢ"},
+            {'T', "ᵀ"},
+            {'U', "ᵁ"},
+            {'V', "ⱽ"},
+            {'W', "ᵂ"},
+            {'X', "ˣ"},
+            {'Y', "ʸ"},
+            {'Z', "ᶻ"},
+            {'+', "⁺"},
+            {'-', "⁻"},
+            {'=', "⁼"},
+            {'.', "⋅"},
+            {'(', "⁽"},
+            {')', "⁾"}
+    };
+
+    static const std::map<char, std::string> charToSubscript {
+            {'0', "₀"},
+            {'1', "₁"},
+            {'2', "₂"},
+            {'3', "₃"},
+            {'4', "₄"},
+            {'5', "₅"},
+            {'6', "₆"},
+            {'7', "₇"},
+            {'8', "₈"},
+            {'9', "₉"},
+            {'a', "ₐ"},
+            {'b', "b"},
+            {'c', "c"},
+            {'d', "d"},
+            {'e', "ₑ"},
+            {'f', "f"},
+            {'g', "g"},
+            {'h', "ₕ"},
+            {'i', "ᵢ"},
+            {'j', "ⱼ"},
+            {'k', "ₖ"},
+            {'l', "ₗ"},
+            {'m', "ₘ"},
+            {'n', "ₙ"},
+            {'o', "ₒ"},
+            {'p', "ₚ"},
+            {'q', "q"},
+            {'r', "ᵣ"},
+            {'s', "ₛ"},
+            {'t', "ₜ"},
+            {'u', "ᵤ"},
+            {'v', "ᵥ"},
+            {'w', "w"},
+            {'x', "ₓ"},
+            {'y', "y"},
+            {'z', "z"},
+            {'A', "ₐ"},
+            {'B', "B"},
+            {'C', "C"},
+            {'D', "D"},
+            {'E', "ₑ"},
+            {'F', "F"},
+            {'G', "G"},
+            {'H', "ₕ"},
+            {'I', "ᵢ"},
+            {'J', "ⱼ"},
+            {'K', "ₖ"},
+            {'L', "ₗ"},
+            {'M', "ₘ"},
+            {'N', "ₙ"},
+            {'O', "ₒ"},
+            {'P', "ₚ"},
+            {'Q', "Q"},
+            {'R', "ᵣ"},
+            {'S', "ₛ"},
+            {'T', "ₜ"},
+            {'U', "ᵤ"},
+            {'V', "ᵥ"},
+            {'W', "W"},
+            {'X', "ₓ"},
+            {'Y', "Y"},
+            {'Z', "Z"},
+            {'+', "₊"},
+            {'-', "₋"},
+            {'.', "."},
+            {'=', "₌"},
+            {'(', "₍"},
+            {')', "₎"}
     };
 
     template<typename I, const std::string* const digitMap>
@@ -106,36 +249,17 @@ namespace tosupsub
         return supsubStr;
     }
 
-    template<const std::string* const digitMap>
-    std::string s_tosupsubscript(const char* const numStr)
+        template<const std::map<char, std::string>& charMap>
+    std::string s_tosupsubscript(const char* const numStr, const size_t length)
     {
-        // TODO: throw exception for num-numeric string?
-
         std::string supsubStr;
-        size_t length = std::strlen(numStr);
         for(size_t i = 0; i < length; ++i)
         {
-            const char ch = numStr[i];
-            switch(ch)
-            {
-                case '+':
-                    supsubStr += digitMap[10];
-                    break;
-                case '-':
-                    supsubStr += digitMap[11];
-                    break;
-                case '.':
-                    supsubStr += digitMap[12];
-                    break;
-                default:
-                    if(ch >= '0' && ch <= '9')
-                        supsubStr += digitMap[ch - '0'];
-                    else
-                        return supsubStr;
-                    break;
-            }
+            if(charMap.count(numStr[i]))
+                supsubStr += charMap.at(numStr[i]);
+            else
+                supsubStr += numStr[i];
         }
-
         return supsubStr;
     }
 
@@ -166,12 +290,12 @@ std::string tosuperscript(const F number,
 
 std::string tosuperscript(const char* const numStr)
 {
-    return tosupsub::s_tosupsubscript<tosupsub::digitToSuperscript>(numStr);
+    return tosupsub::s_tosupsubscript<tosupsub::charToSuperscript>(numStr, std::strlen(numStr));
 }
 
 std::string tosuperscript(const std::string& numStr)
 {
-    return tosupsub::s_tosupsubscript<tosupsub::digitToSuperscript>(numStr.data());
+    return tosupsub::s_tosupsubscript<tosupsub::charToSuperscript>(numStr.data(), numStr.length());
 }
 
 template<class SI>
@@ -199,12 +323,12 @@ std::string tosubscript(const F number,
 
 std::string tosubscript(const char* const numStr)
 {
-    return tosupsub::s_tosupsubscript<tosupsub::digitToSubscript>(numStr);
+    return tosupsub::s_tosupsubscript<tosupsub::charToSubscript>(numStr, std::strlen(numStr));
 }
 
 std::string tosubscript(const std::string& numStr)
 {
-    return tosupsub::s_tosupsubscript<tosupsub::digitToSubscript>(numStr.data());
+    return tosupsub::s_tosupsubscript<tosupsub::charToSubscript>(numStr.data(), numStr.length());
 }
 
 #endif //TOSUPSUB_TOSUPSUB_HPP
